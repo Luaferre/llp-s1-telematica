4334af6efda026986733701dec9a58475761ab96
flferreira <127799259+Luaferre@users.noreply.github.com>
2023-04-12 18:05:58 -0300

    Update Atividade-01.c

--- Atividades/Atividade-01/Atividade-01.c
+++ Atividades/Atividade-01/Atividade-01.c
@@ -30,3 +30,32 @@ srand(time (NULL));
 
     return 0;
 }
+
+//2) Calcula a matriz soma de duas matrizes quadradas.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+    int main() {
+    int mat1 [5][5] = {2,4,6,8,10, 2,4,6,8,10, 2,4,6,8,10, 2,4,6,8,10, 2,4,6,8,10};
+        printf("\n\n");
+    int mat2 [5][5] = {1,3,5,7,9, 1,3,5,7,9, 1,3,5,7,9, 1,3,5,7,9, 1,3,5,7,9};
+    int resultante [5][5];
+        for (int i=0; i<5; i++) {
+            for (int j=0; j<5; j++) {
+                resultante [i][j] = mat1[i][j]+mat2[i][j];
+  }
+
+}
+printf("\n\n Matriz soma: \n");
+for(int mat1=0; mat1<5; mat1++) {
+printf("\n\n");
+  for(int mat2=0; mat2<5; mat2++) {
+printf("%d, ", resultante [mat1][mat2]);
+
+  }
+}
+return 0;
+}
